24e376ac1eaec8a98484ae81a74bbeee8e5ccf91
Dev 4 <alaster.rom@gmail.com>
2020-02-17 09:02:15 +0200

    add file

--- Client_Window.java
+++ Client_Window.java
@@ -15,9 +15,4 @@ public class AddTwoNumbers {
 		System.out.println("Del of these numbers: " + del);
 	}
 	
-	public static void main(String[] args) {
-		int num1 = 5, num2 = 15, del;
-		del = num1 + num2;
-		System.out.println("Del of these numbers: " + del);
-	}
 }
