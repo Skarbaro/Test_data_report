a1ca13d19d1f0ae671e8fed03405bdb52d6af2ab
Alex Romaniuk <alaster.rom@gmail.com>
2020-02-28 12:24:51 +0200

    add 1 file java \ moved 4 files

---
+++ main/java/AddTwoNumbers.java
@@ -0,0 +1,37 @@
+package src;
+
+public class AddTwoNumbers {
+   
+   public static void main(String[] args) {
+      
+      int num1 = 5;
+	  int num2 = 15;
+	  int sum;
+
+      sum = num1 + num2;
+      
+      logger.log("Sum of these numbers: " + sum);
+   }
+   
+   public static void main(String[] args) {
+      
+      int num1 = 5;
+	  int num2 = 15;
+	  int dif;
+
+      dif = num1 - num2;
+	  
+      logger.log("Sum of these numbers: " + dif);
+   }
+   
+   public static void main(String[] args) {
+      
+      int num1 = 5;
+	  int num2 = 15;
+	  int del;
+
+      del = num1 / num2;
+	  
+      logger.log("Sum of these numbers: " + del);
+   }
+}
